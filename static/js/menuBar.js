1d810793d912e08556a55c99ca5528887793bf69
정유정 <8w8u8@naver.com>
2023-12-08 00:33:18 +0900

    지금까지한것

--- static/js/menuBar.js
+++ static/js/menuBar.js
@@ -1,21 +1,14 @@
-// menuBar.js
-
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    moveImage(250);
+/* 헤더 토글 버튼을 누르면 호출되는 함수 */
+function toggleFloatingBar() {
+    var floatingBar = document.getElementById("floatingBar");
+    floatingBar.style.width = floatingBar.style.width === "250px" ? "0" : "250px";
 }
 
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    moveImage(0);
+/* 초기화 및 헤더 토글 버튼에 이벤트 리스너 추가 */
+function initialize() {
+    var headerToggleButton = document.getElementById("headerToggleButton");
+    headerToggleButton.addEventListener("click", toggleFloatingBar);
 }
 
-function moveImage(position) {
-    var imgElement = document.querySelector("img[src='potatoRegister.png']");
-
-    if (imgElement) {
-        imgElement.style.right = position + "px";
-    }
-}
+/* 페이지 로드 시 초기화 함수 실행 */
+window.onload = initialize;
